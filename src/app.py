eae8682547a25b510e91cb02546b5c49b66b4b2e
Daniil Frangov <frangov7@gmail.com>
2021-11-21 22:06:26 +0300

    Add test handler

--- src/app.py
+++ src/app.py
@@ -1,4 +1,21 @@
-class App:
+import os
+import asyncio
 
-    def run():
-        pass
+from aiogram import Bot, Dispatcher
+from handler import *
+
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    try:
+        disp = Dispatcher(bot=bot)
+        disp.register_message_handler(
+            start_handler, commands={"start", "restart"})
+        await disp.start_polling()
+    finally:
+        await bot.close()
+
+asyncio.run(main())
---
+++ src/handler.py
@@ -0,0 +1,8 @@
+from aiogram import types 
+
+
+async def start_handler(event: types.Message):
+    await event.answer(
+        f"Hello, {event.from_user.get_mention(as_html=True)} 👋!",
+        parse_mode=types.ParseMode.HTML,
+    )
