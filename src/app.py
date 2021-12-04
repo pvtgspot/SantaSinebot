4e6aa0c941c40ca3693806ded70d6ed9fe107eb4
Daniil Frangov <frangov7@gmail.com>
2021-12-05 02:03:27 +0300

    Refactor App class

--- src/app.py
+++ src/app.py
@@ -1,21 +1,31 @@
 import os
 import asyncio
 
+from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from handler import *
 
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+class App:
 
+    def __init__(self):
+        load_dotenv()
 
-async def main():
-    bot = Bot(token=BOT_TOKEN)
-    try:
-        disp = Dispatcher(bot=bot)
-        disp.register_message_handler(
-            start_handler, commands={"start", "restart"})
-        await disp.start_polling()
-    finally:
-        await bot.close()
+        self.BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-asyncio.run(main())
+        STORAGE_FILE = os.environ.get("STORAGE_FILE")
+        with open(STORAGE_FILE, "a+"):
+            pass
+
+    async def _process(self):
+        bot = Bot(token=self.BOT_TOKEN)
+        try:
+            disp = Dispatcher(bot=bot)
+            disp.register_message_handler(
+                start_handler, commands={"start", "restart"})
+            await disp.start_polling()
+        finally:
+            await bot.close()
+
+    def run(self):
+        asyncio.run(self._process())
