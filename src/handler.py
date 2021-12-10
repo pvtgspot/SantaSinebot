6e3b02d969a89ee6f83cfc185879105901df6bd3
Gleb <bakhmetyevgv@gmail.com>
2021-12-10 20:30:13 +0300

    Add Handlers

--- src/handler.py
+++ src/handler.py
@@ -3,6 +3,9 @@ import os
 from pathlib import Path
 from aiogram import types
 from util import get_storage_file_path
+from disorders import disorder
+from participants import save_participants_to, load_participants_from
+
 
 
 def read_ids(storage_file):
@@ -52,10 +55,62 @@ async def start_handler(event: types.Message):
 async def shuffle_handler(event: types.Message):
     STORAGE_PATH = get_storage_file_path()
 
-    user_id = event.from_user.id
+    MASTER_IDS = [
+        261135959, # Daniil Frangov
+        426142808  # Gleb Bakhmetyev
+    ]
 
     no_access_msg = "У вас нет прав на выполнение данной операции"
-    success_msg = "ты теперь в списке участников Секретного Сасанты..."
+    success_msg = "Распределение сасантов успешно выполнено"
+
+    user_id = event.from_user.id
+
+    if MASTER_IDS.count(user_id) == 0:
+        await event.answer(
+            no_access_msg,
+            parse_mode=types.ParseMode.HTML,
+        )
+        
+        return
+
+    
+    with open(STORAGE_PATH, "r+") as f:
+        ids = read_ids(f)
+        
+        shuffle = disorder(ids)    
+        shuffle_ids = map(lambda x:str(x), shuffle)
+
+        save_participants_to("shuffle", shuffle_ids)
+
+
+    await event.answer(
+        success_msg,
+        parse_mode=types.ParseMode.HTML,
+    )
+
+async def getsome_handler(event: types.Message):
+    STORAGE_PATH = get_storage_file_path()
+
+    user_id = event.from_user.id
+
+    msg = "Ты даришь подарок "
+
+    ids = []
+    with open(STORAGE_PATH, "r+") as f:
+        ids = read_ids(f)
+
+    shuffle_ids = load_participants_from("shuffle")
+
+    user_id_str = str(user_id)
+
+    i = ids.index(user_id_str)
+
+    sasanta_id = int(shuffle_ids[i])
+
+    sasanta_user = users.getFullUser(sasanta_id) # TODO
+
+    sasanta_mention = sasanta_user.get_mention(as_html=True)
+    msg = msg + sasanta_mention
 
     await event.answer(
         msg,
